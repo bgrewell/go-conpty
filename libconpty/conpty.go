136a42e0c0ab79d65f67c7366f8aa99e57cf99b7
Ben Grewell <benjamin.grewell@intel.com>
2021-08-04 14:55:45 -0700

    simplify conpty code

--- libconpty/conpty.go
+++ libconpty/conpty.go
@@ -1,9 +1,12 @@
 package libconpty
 
 import (
+	"encoding/csv"
 	"fmt"
 	. "github.com/bgrewell/go-conpty/libconpty/types"
 	"golang.org/x/sys/windows"
+	"os/exec"
+	"strings"
 	"syscall"
 	"unsafe"
 )
@@ -25,15 +28,34 @@ var (
 	fHeapAlloc						   = win32.NewProc("HeapAlloc")
 )
 
+type IOHandle struct {
+	handle syscall.Handle
+}
+
+func (h *IOHandle) Read(p []byte) (int, error) {
+	n := uint32(0)
+	err := syscall.ReadFile(h.handle, p, &n, nil)
+	return int(n), err
+}
+
+func (h *IOHandle) Write(p []byte) (int, error) {
+	n := uint32(0)
+	err := syscall.WriteFile(h.handle, p, &n, nil)
+	return int(n), err
+}
+
+func (h *IOHandle) Close() error {
+	return syscall.CloseHandle(h.handle)
+}
+
 type ConPty struct {
 	hPC         HPCON `json:"handle_pseudoconsole"` // Handle to pseudo console device
-	ptyIn       windows.Handle
-	ptyOut      windows.Handle
-	cmdIn       windows.Handle
-	cmdOut      windows.Handle
+	ptyIn       syscall.Handle
+	ptyOut      syscall.Handle
+	cmdIn       syscall.Handle
+	cmdOut      syscall.Handle
 	cmd         string
 	consoleSize COORD
-	pi          *windows.ProcessInformation
 }
 
 func NewConPty(cmd string, cols int16, rows int16) (conpty *ConPty, err error) {
@@ -59,104 +81,124 @@ func (c *ConPty) Initialize() (err error) {
 		return err
 	}
 
+	// Execute process
+	r := csv.NewReader(strings.NewReader(c.cmd))
+	r.Comma = ' '
+	parts, err := r.Read()
+	if err != nil {
+		return err
+	}
+	exe, err := exec.LookPath(parts[0])
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command(exe, parts[1:]...)
+	cmd.Stdin = &IOHandle{c.ptyIn}
+	cmd.Stdout = &IOHandle{c.ptyOut}
+	cmd.Stderr = &IOHandle{c.ptyOut}
+
 	// Create pseudo console
 	if err = c.createPseudoConsole(); err != nil {
 		return err
 	}
 
 	// Close the handles (they are already dup'ed into the ConHost)
-	if err = c.closePtyHandles(); err != nil {
-		return err
-	}
-
-	// Initialize startup info
-	var si *StartupInfoEx
-	if si, err = c.initStartupInfoEx(); err != nil {
-		return err
-	}
-
-	// Create process
-	return c.createProcess(si)
-
-}
-
-func (c *ConPty) createProcess(si *StartupInfoEx) error {
-	cmdline, err := windows.UTF16PtrFromString(c.cmd)
-	if err != nil {
-		return err
-	}
-	c.pi = new(windows.ProcessInformation)
-	err = windows.CreateProcess(
-		nil,                                  	// _in_opt_			LPCTSTR
-		cmdline,                              			// _Inout_opt_		LPTSTR
-		nil,                                  // _In_opt_			LPSECURITY_ATTRIBUTES
-		nil,                                 // _In_opt_		LPSECURITY_ATTRIBUTES
-		false,                               // _In_			BOOL
-		windows.EXTENDED_STARTUPINFO_PRESENT, 			// _In_ 			DWORD
-		nil,                                  		// _In_opt_			LPVOID
-		nil,                                  	// _In_opt_			LPCTSTR
-		&si.StartupInfo,                      			// _In_				LPSTARTUPINFO
-		c.pi,                                 			// _Out_
-	)
-	if err != nil {
-		return err
-	}
-
-	//event, err := windows.WaitForSingleObject(c.pi.Thread, 500)
-	_, err = windows.WaitForSingleObject(c.pi.Thread, 5000)
-	if err != nil {
-		return err
-	}
-	//if event != 0x0 {
-	//	fmt.Println("WaitForSingleObject returned unexpected value: %d", event)
+	//if err = c.closePtyHandles(); err != nil {
+	//	return err
 	//}
-	return nil
+	//
+	// Initialize startup info
+	//var si *StartupInfoEx
+	//if si, err = c.initStartupInfoEx(); err != nil {
+	//	return err
+	//}
+	//
+	// Create process
+	//return c.createProcess(si)
+	return cmd.Start()
 }
 
-func (c *ConPty) initStartupInfoEx() (si *StartupInfoEx, err error) {
-	if fInitializeProcThreadAttributeList.Find() != nil {
-		return nil, fmt.Errorf("Unsupported version of Windows. InitializeProcThreadAttributeList not found")
-	}
-	if fUpdateProcThreadAttribute.Find() != nil {
-		return nil, fmt.Errorf("Unsupported version of Windows. UpdateProcThreadAttribute not found")
-	}
-	si = &StartupInfoEx{}
-	si.StartupInfo.Cb = uint32(unsafe.Sizeof(StartupInfoEx{}))
-	lpSize := uint32(0)
-	fInitializeProcThreadAttributeList.Call(0, 1, 0, uintptr(unsafe.Pointer(&lpSize)))
-
-	heap, _, err := fGetProcessHeap.Call()
-	if err != syscall.Errno(0) {
-		return nil, fmt.Errorf("Failed to get process heap: %v", err)
-	}
-	const heapZeroMem = 0x00000008
-	si.AttributeList, _, err = fHeapAlloc.Call(heap, heapZeroMem, uintptr(lpSize))
-	if err != syscall.Errno(0) {
-		return nil, fmt.Errorf("Failed to allocate space on the heap: %v", err)
-	}
-	ret, _, err := fInitializeProcThreadAttributeList.Call(uintptr(unsafe.Pointer(si.AttributeList)), 1, 0, uintptr(unsafe.Pointer(&lpSize)))
-	if ret != 1 {
-		return nil, fmt.Errorf("Failed to initialize thread attribute list: %v", err)
-	}
-	ret, _, err = fUpdateProcThreadAttribute.Call(uintptr(unsafe.Pointer(si.AttributeList)),
-		0,
-		PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE,
-		uintptr(c.hPC),
-		unsafe.Sizeof(c.hPC),
-		0,
-		0)
-	if ret != 1 {
-		return nil, fmt.Errorf("Failed to update thread attribute list: %v", err)
-	}
-
-	return si, nil
-}
+//func (c *ConPty) createProcess(si *StartupInfoEx) error {
+//	cmdline, err := windows.UTF16PtrFromString(c.cmd)
+//	if err != nil {
+//		return err
+//	}
+//	c.pi = new(windows.ProcessInformation)
+//	err = windows.CreateProcess(
+//		nil,                                  	// _in_opt_			LPCTSTR
+//		cmdline,                              			// _Inout_opt_		LPTSTR
+//		nil,                                  // _In_opt_			LPSECURITY_ATTRIBUTES
+//		nil,                                 // _In_opt_		LPSECURITY_ATTRIBUTES
+//		false,                               // _In_			BOOL
+//		windows.EXTENDED_STARTUPINFO_PRESENT, 			// _In_ 			DWORD
+//		nil,                                  		// _In_opt_			LPVOID
+//		nil,                                  	// _In_opt_			LPCTSTR
+//		&si.StartupInfo,                      			// _In_				LPSTARTUPINFO
+//		c.pi,                                 			// _Out_
+//	)
+//	if err != nil {
+//		return err
+//	}
+//
+//	//event, err := windows.WaitForSingleObject(c.pi.Thread, 500)
+//	_, err = windows.WaitForSingleObject(c.pi.Thread, 5000)
+//	if err != nil {
+//		return err
+//	}
+//	//if event != 0x0 {
+//	//	fmt.Println("WaitForSingleObject returned unexpected value: %d", event)
+//	//}
+//	return nil
+//}
+//
+//func (c *ConPty) initStartupInfoEx() (si *StartupInfoEx, err error) {
+//	if fInitializeProcThreadAttributeList.Find() != nil {
+//		return nil, fmt.Errorf("Unsupported version of Windows. InitializeProcThreadAttributeList not found")
+//	}
+//	if fUpdateProcThreadAttribute.Find() != nil {
+//		return nil, fmt.Errorf("Unsupported version of Windows. UpdateProcThreadAttribute not found")
+//	}
+//	si = &StartupInfoEx{}
+//	si.StartupInfo.Cb = uint32(unsafe.Sizeof(StartupInfoEx{}))
+//	var lpSize uintptr
+//	r1, _, err := fInitializeProcThreadAttributeList.Call(0, 1, 0, uintptr(unsafe.Pointer(&lpSize)))
+//	if err != nil && r1 == 0x0 {
+//		// it's safe to ignore the data area passed too small error that error is returned by design
+//		//log.Printf("InitializeProcThreadAttributeList error: %v\n", err)
+//	}
+//
+//	heap, _, err := fGetProcessHeap.Call()
+//	if err != syscall.Errno(0) {
+//		return nil, fmt.Errorf("Failed to get process heap: %v", err)
+//	}
+//	const heapZeroMem = 0x00000008
+//	si.AttributeList, _, err = fHeapAlloc.Call(heap, heapZeroMem, uintptr(lpSize))
+//	if err != syscall.Errno(0) {
+//		return nil, fmt.Errorf("Failed to allocate space on the heap: %v", err)
+//	}
+//	ret, _, err := fInitializeProcThreadAttributeList.Call(uintptr(unsafe.Pointer(si.AttributeList)), 1, 0, uintptr(unsafe.Pointer(&lpSize)))
+//	if ret == 0x0 {
+//		log.Fatalf("Failed to initialize thread attribute list: %v", err)
+//	}
+//	ret, _, err = fUpdateProcThreadAttribute.Call(uintptr(unsafe.Pointer(si.AttributeList)),
+//		0,
+//		PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE,
+//		uintptr(c.hPC),
+//		unsafe.Sizeof(c.hPC),
+//		0,
+//		0)
+//	if ret != 1 {
+//		return nil, fmt.Errorf("Failed to update thread attribute list: %v", err)
+//	}
+//
+//	return si, nil
+//}
 
 func (c *ConPty) closePtyHandles() error {
-	if err := windows.CloseHandle(c.ptyIn); err != nil {
+	if err := syscall.CloseHandle(c.ptyIn); err != nil {
 		return err
 	}
-	if err := windows.CloseHandle(c.ptyOut); err != nil {
+	if err := syscall.CloseHandle(c.ptyOut); err != nil {
 		return err
 	}
 	return nil
@@ -179,10 +221,10 @@ func (c *ConPty) createPseudoConsole() error {
 }
 
 func (c *ConPty) setupPipes() error {
-	if err := windows.CreatePipe(&c.ptyIn, &c.cmdIn, nil, 0); err != nil {
+	if err := syscall.CreatePipe(&c.ptyIn, &c.cmdIn, nil, 0); err != nil {
 		return err
 	}
-	if err := windows.CreatePipe(&c.cmdOut, &c.ptyOut, nil, 0); err != nil {
+	if err := syscall.CreatePipe(&c.cmdOut, &c.ptyOut, nil, 0); err != nil {
 		return err
 	}
 	return nil
@@ -192,26 +234,22 @@ func (c *ConPty) dataAvailable() (bytesAvailable int, err error) {
 	if fPeekNamedPipe.Find() != nil {
 		return -1, fmt.Errorf("Unsupported version of Windows. PeekNamedPipe not found")
 	}
-	maxRead := 1024 * 8
-	numRead := uint32(0)
-	numAvail := uint32(0)
-	numLeft := uint32(0)
-	buf := make([]byte, maxRead)
-	_p0 := &buf[0]
+	var numAvail uint32
 
-	ret, _, err := fPeekNamedPipe.Call(uintptr(c.cmdOut),
-									   uintptr(unsafe.Pointer(_p0)),
-									   uintptr(len(buf)),
-									   uintptr(unsafe.Pointer(&numRead)),
+	ret, _, err := fPeekNamedPipe.Call(uintptr(unsafe.Pointer(c.cmdOut)),
+									   0,
+									   0,
+									   0,
 									   uintptr(unsafe.Pointer(&numAvail)),
-									   uintptr(unsafe.Pointer(&numLeft)))
-	if err != nil || ret == 0x0 {
+									   0)
+	if ret == 0x0 {
 		return -1, err
 	}
 	return int(numAvail), nil
 }
 
 func (c *ConPty) Read(p []byte) (n int, err error) {
+	// TODO: Figure out why this isn't working anymore
 	if avail, err := c.dataAvailable(); avail <= 0 || err != nil {
 		if err != nil && err != syscall.Errno(0) {
 			return 0, err
@@ -219,7 +257,7 @@ func (c *ConPty) Read(p []byte) (n int, err error) {
 		return 0, nil
 	}
 	numRead := uint32(0)
-	err = windows.ReadFile(c.cmdOut, p, &numRead, nil)
+	err = syscall.ReadFile(c.cmdOut, p, &numRead, nil)
 	if err != nil && err != syscall.Errno(0) {
 		return 0, err
 	}
@@ -228,7 +266,7 @@ func (c *ConPty) Read(p []byte) (n int, err error) {
 
 func (c *ConPty) Write(p []byte) (n int, err error) {
 	numWritten := uint32(0)
-	err = windows.WriteFile(c.cmdIn, p, &numWritten, nil)
+	err = syscall.WriteFile(c.cmdIn, p, &numWritten, nil)
 	if err != nil && err != syscall.Errno(0) {
 		return 0, err
 	}
@@ -236,11 +274,9 @@ func (c *ConPty) Write(p []byte) (n int, err error) {
 }
 
 func (c *ConPty) Close() {
-	windows.CloseHandle(c.cmdIn)
-	windows.CloseHandle(c.cmdOut)
+	syscall.CloseHandle(c.cmdIn)
+	syscall.CloseHandle(c.cmdOut)
 	// todo: need to implement DeleteProcThreadAttributeList to cleanup if garbage collector doesn't get it
 	// fDeleteProcThreadAttributeList(c.si.lpAttributeList)
 	fClosePseudoConsole.Call(uintptr(c.hPC))
-	windows.CloseHandle(c.pi.Thread)
-	windows.CloseHandle(c.pi.Process)
 }
--- main.go
+++ main.go
@@ -4,38 +4,69 @@ import (
 	"fmt"
 	"github.com/bgrewell/go-conpty/libconpty"
 	"log"
+	"time"
 )
+
+func read(pty *libconpty.ConPty, cancel chan bool) {
+	data := make([]byte, 1024)
+
+	for {
+		select {
+			case <- cancel:
+				return
+			default:
+				n, err := pty.Read(data)
+				if err != nil {
+					// who cares
+				}
+				if n > 0 {
+					fmt.Printf("%s", string(data[:n]))
+				}
+		}
+	}
+}
 
 // main is a simple test application to check the functionality of libconpty. This is not an
 // automated test, it is simply a way for a developer to manually test functionality.
 func main() {
 
-	pty, err := libconpty.NewConPty("cmd.exe", 90, 60)
+	pty, err := libconpty.NewConPty("powershell.exe -NoProfile", 90, 60)
 	if err != nil {
 		log.Fatalf("failed to get new ConPty: %v\n", err)
 	}
-	data := make([]byte, 1024)
-	n, err := pty.Read(data)
-	fmt.Printf("bytes read: %d\n", n)
-	if err != nil {
-		log.Fatalf("failed to read from ConPty: %v\n", err)
-	} else {
-		fmt.Println(string(data[:n]))
-	}
 
-	n, err = pty.Write([]byte("whoami\r\n"))
-	fmt.Printf("bytes written: %d\n", n)
-	if err != nil {
-		log.Fatalf("failed to write to ConPty: %v\n", err)
-	}
+	cancel := make(chan bool)
+	go read(pty, cancel)
 
-	n, err = pty.Read(data)
-	fmt.Printf("bytes read: %d\n", n)
-	if err != nil {
-		log.Fatalf("failed to read from ConPty: %v\n", err)
-	} else {
-		fmt.Println(string(data[:n]))
-	}
+	pty.Write([]byte("whoami\r\n"))
+
+	time.Sleep(30 * time.Second)
+
+	cancel <- true
+	pty.Close()
+
+	//data := make([]byte, 1024)
+	//n, err := pty.Read(data)
+	//fmt.Printf("bytes read: %d\n", n)
+	//if err != nil {
+	//	log.Fatalf("failed to read from ConPty: %v\n", err)
+	//} else {
+	//	fmt.Println(string(data[:n]))
+	//}
+	//
+	//n, err = pty.Write([]byte("whoami\r\n"))
+	//fmt.Printf("bytes written: %d\n", n)
+	//if err != nil {
+	//	log.Fatalf("failed to write to ConPty: %v\n", err)
+	//}
+	//
+	//n, err = pty.Read(data)
+	//fmt.Printf("bytes read: %d\n", n)
+	//if err != nil {
+	//	log.Fatalf("failed to read from ConPty: %v\n", err)
+	//} else {
+	//	fmt.Println(string(data[:n]))
+	//}
 
 	pty.Close()
 }
